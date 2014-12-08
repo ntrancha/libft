a5b0e5b8cc5b9013fc36fcc193237133f8891e4d
nk <nk-server@orange.fr>
2014-12-08 07:27:35 +0100

    ft_nbrlen

--- includes/libft.h
+++ includes/libft.h
@@ -47,6 +47,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(char *str);
+size_t	ft_nbrlen(int nbr);
 char	*ft_strdup(char *str);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncpy(char *dst, const char *src, size_t n);
