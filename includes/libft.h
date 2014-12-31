e9b9c2361659bb6800c193505b737c3654c78590
nk <nk-server@orange.fr>
2015-01-01 00:13:39 +0100

    header del

--- includes/libft.h
+++ includes/libft.h
@@ -12,12 +12,46 @@
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
 # include <string.h>
-# include "macros.h"
-# include "math.h"
-# include "put.h"
-# include "file.h"
-# include "to.h"
+
+# define BUFF_SIZE		10
+# define TRUE			1
+# define FALSE			0
+# define STDIN			0
+# define STDOUT			1
+# define STDERR			2
+# define MAX_DTOA		10000000000000
+# define MAX_FTOA		10000000
+# define PI				3.141593
+# define PHI			1.618032
+# define SQUARE2		1.414213
+# define RETURN_SUCCES	return (EXIT_SUCCES);
+# define RETURN_NULL	return (EXIT_NULL);
+# define RETURN_FAIL	return (EXIT_FAIL);
+# define RETURN_ERROR	return (EXIT_ERROR);
+# define EXIT_SUCCES	1
+# define EXIT_NULL		0
+# define EXIT_FAIL		-1
+# define EXIT_ERROR		-2
+# define USER_OS		OS_UNKNOW
+# define OS_UNKNOW
+# define USER_SYS		SYS_UNKNOW
+# define SYS_UNKNOW
+# define PUT_ENDL		ft_putchar('\n')
+# define PUT_ERROR(x)	ft_putstr_fd(x, STDERR)
+# define PUT_ERROR_L(x)	ft_putendl_fd(x, STDERR)
+# define STR(x)			ft_strdup(#x)
+# define CONTENT		ft_listcontent
+# define POWER(x, y)	ft_power(x, y)
+# define FACTO(x)		ft_facto(x)
+# define SQUARE(x)		ft_sqrt(x)
+# define ROOT(x)		ft_rsqrt(x)
+# define SIN(x)			ft_sin(x)
+# define COS(x)			ft_cos(x)
+# define TAN(x)			ft_tan(x)
+# define ABS(x)			ft_abs(x)
+# define NEG(x)			ft_neg(x)
 
 typedef unsigned char	t_uchar;
 typedef unsigned int	t_uint;
@@ -61,6 +95,57 @@ typedef struct          s_list
     t_node				*end;
 	int					size;
 }						t_list;
+
+
+int		ft_abs(int n);
+int		ft_find_next_prime(int nb);
+int		ft_neg(int nbr);
+int		ft_power(int nbr, int power);
+int		ft_sqrt(int nbr);
+int		ft_facto(int nb);
+long	ft_abs_long(long n);
+long	ft_neg_long(long nbr);
+t_ll	ft_abs_longlong(long long n);
+float	ft_abs_float(float n);
+float	ft_neg_float(float nbr);
+float	ft_rsqrt(float nb);
+float	ft_cos(float angle);
+float	ft_sin(float angle);
+float	ft_tan(float angle);
+double	ft_abs_double(double n);
+double	ft_neg_double(int nbr);
+
+char	*ft_itoa(int n);
+char	*ft_ltoa(long n);
+char	*ft_ftoa(float n);
+char	*ft_dtoa(double n);
+char	*ft_ctos(char c);
+char	*ft_listtostr(t_list *list);
+char	*ft_listtostrd(t_list *list, char *del);
+int		ft_atoi(const char *str);
+int		ft_ltoi(long n);
+int		ft_ftoi(float n);
+int		ft_dtoi(double n);
+int		ft_dectobin(int dec);
+int		ft_bintodec(int bin);
+long	ft_atol(const char *str);
+long	ft_itol(int n);
+long	ft_dtol(double n);
+long	ft_ftol(float n);
+t_ll	ft_itoll(int n);
+t_ll	ft_ltoll(long n);
+t_ll	ft_dtoll(double n);
+t_ll	ft_ftoll(float n);
+t_ll	ft_atoll(const char *str);
+float	ft_atof(const char *str);
+float	ft_itof(int n);
+float	ft_ltof(long n);
+float	ft_dtof(double n);
+float	ft_fround(float n, int p);
+double	ft_atod(const char *str);
+double	ft_itod(int n);
+double	ft_ltod(long n);
+double	ft_ftod(float n);
 
 int		ft_isdigit(int c);
 int		ft_isalpha(int c);
@@ -182,5 +267,21 @@ t_list	*ft_listreverse(t_list *list);
 int		ft_tabmin(t_tab *tab);
 int		ft_tabmax(t_tab *tab);
 void	ft_tabup(t_tab *tab, int up);
+
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putendl_fd(char const *s, int fd);
+
+void	ft_putchar(char c);
+void	ft_putstr(char const *str);
+void	ft_putendl(char const *s);
+void	ft_putnbr(int n);
+void	ft_putnbr_endl(int n);
+void	ft_putfloat(float n);
+void	ft_putlong(long n);
+void	ft_putdouble(double n);
+void	ft_listputstr(t_list *list, void (put)(char const *));
+int		ft_puttab(t_tab *tab);
 
 #endif
