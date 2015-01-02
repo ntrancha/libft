82e2cfbd36ec902b49722f80d4737fd9d8844df8
nk <nk-server@orange.fr>
2015-01-02 18:10:54 +0100

    memory.h

--- includes/memory.h
+++ includes/memory.h
@@ -14,16 +14,16 @@
 # define MEMORY_H
 # include "libft.h"
 
-void    *ft_memchr(const void *s, int c, size_t n);
-void    *ft_memcpy(void *dst, const void *src, size_t n);
-void    *ft_memccpy(void *dst, const void *src, int c, size_t n);
-void    *ft_memmove(void *dst, const void *src, size_t len);
-void    *ft_ralloc(void *ptr, size_t size_of, size_t new_size);
-void    *ft_memset(void *b, int c, size_t len);
-void    ft_bzero(void *s, size_t n);
-void    *ft_memalloc(size_t size);
-void    ft_memdel(void **ap);
-int     ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_ralloc(void *ptr, size_t size_of, size_t new_size);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memalloc(size_t size);
+void	ft_memdel(void **ap);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 
 #endif
