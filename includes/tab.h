8adb1aef5a2f2988fded6ea028f9dde34f7dc813
nk <nk-server@orange.fr>
2015-01-02 18:15:10 +0100

    tab.h

--- includes/tab.h
+++ includes/tab.h
@@ -14,20 +14,20 @@
 # define TAB_H
 # include "libft.h"
 
-typedef struct          s_tab
+typedef struct	s_tab
 {
-    int                 **tab;
-    int                 col;
-    int                 line;
-}                       t_tab;
+	int			**tab;
+	int			col;
+	int			line;
+}				t_tab;
 
-int     ft_tabmin(t_tab *tab);
-int     ft_tabmax(t_tab *tab);
-void    ft_tabup(t_tab *tab, int up);
-t_tab   *ft_tabdup(t_tab *tab);
-int     ft_puttab(t_tab *tab);
-void    ft_tabclear(t_tab **tab);
-t_tab   *ft_tabdel(t_tab **tab);
-t_tab   *ft_tabnew(int line, int col);
+int		ft_tabmin(t_tab *tab);
+int		ft_tabmax(t_tab *tab);
+void	ft_tabup(t_tab *tab, int up);
+t_tab	*ft_tabdup(t_tab *tab);
+int		ft_puttab(t_tab *tab);
+void	ft_tabclear(t_tab **tab);
+t_tab	*ft_tabdel(t_tab **tab);
+t_tab	*ft_tabnew(int line, int col);
 
 #endif
