7dd5d6d2eef3ebab9cb57c5ece4bdf1c3e44c122
nk <nk-server@orange.fr>
2015-01-02 18:16:30 +0100

    test.h

--- includes/test.h
+++ includes/test.h
@@ -14,14 +14,14 @@
 # define TEST_H
 # include "libft.h"
 
-int     ft_isdigit(int c);
-int     ft_isalpha(int c);
-int     ft_isalnum(int c);
-int     ft_isascii(int c);
-int     ft_isprint(int c);
-int     ft_istolower(int c);
-int     ft_istoupper(int c);
-int     ft_isspace(char c);
-int     ft_is_prime(int nbr);
+int	ft_isdigit(int c);
+int	ft_isalpha(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_istolower(int c);
+int	ft_istoupper(int c);
+int	ft_isspace(char c);
+int	ft_is_prime(int nbr);
 
 #endif
