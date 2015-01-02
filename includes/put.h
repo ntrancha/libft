475ab4b0b1fbf254b101893334fefb7858dcab56
nk <nk-server@orange.fr>
2015-01-02 18:11:36 +0100

    put.h

--- includes/put.h
+++ includes/put.h
@@ -14,18 +14,17 @@
 # define PUT_H
 # include "libft.h"
 
-void    ft_putnbr_fd(int n, int fd);
-void    ft_putchar_fd(char c, int fd);
-void    ft_putstr_fd(char const *s, int fd);
-void    ft_putendl_fd(char const *s, int fd);
-
-void    ft_putchar(char c);
-void    ft_putstr(char const *str);
-void    ft_putendl(char const *s);
-void    ft_putnbr(int n);
-void    ft_putnbr_endl(int n);
-void    ft_putfloat(float n);
-void    ft_putlong(long n);
-void    ft_putdouble(double n);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putendl_fd(char const *s, int fd);
+void	ft_putchar(char c);
+void	ft_putstr(char const *str);
+void	ft_putendl(char const *s);
+void	ft_putnbr(int n);
+void	ft_putnbr_endl(int n);
+void	ft_putfloat(float n);
+void	ft_putlong(long n);
+void	ft_putdouble(double n);
 
 #endif
