7623f53a21e4d0adc85ee5d35cd4d1e7ac00dd0b
Ntrancha <101010.nk@gmail.com>
2015-08-31 23:21:09 +0200

    select_lib

--- conf/select_lib.c
+++ conf/select_lib.c
@@ -6,7 +6,7 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/31 11:25:47 by ntrancha          #+#    #+#             */
-/*   Updated: 2015/08/31 11:51:38 by ntrancha         ###   ########.fr       */
+/*   Updated: 2015/08/31 23:17:58 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,30 +58,68 @@ void	display_list(char **list)
 			ft_putendl(list[index]);
 }
 
+int         is_src(char *option, char **src)
+{
+    int     ret;
+
+    ret = -1;
+    while (src[++ret])
+        if (ft_strcmp(src[ret], option) == 0)
+            return (1);
+    return (0);
+}
+
+char        **recup_src(t_opt *options, char **src)
+{
+    t_list  *list;
+    char    **tab;
+    char    *option;
+
+    list = ft_listcreate();
+    option = ft_optgetopt_next(options);
+    while (option)
+    {
+        if (is_src(option, src) == 1)
+            ft_listadd(list, (void*)option);
+        option = ft_optgetopt_next(options);
+    }
+    tab = ft_listtotab(list);
+    ft_listdel(list, ft_memdel);
+    return (tab);
+}
+
+char    **recup_list(t_opt *options, char **list_all)
+{
+	char	*option;
+	char	**list;
+
+	option = ft_optgetopt_simple(options, "-a");
+	if (!option)
+        return (recup_src(options, list_all));
+	ft_strdel(&option);
+    return (list_src());
+}
+
 int		main(int argc, char **argv)
 {
 	t_opt	*options;
 	char	*option;
+	char	**list_all;
 	char	**list;
-	int		all;
 
+    list_all = list_src();
 	options = ft_optget(argc, argv);
-	list = list_src();
 	option = ft_optgetopt_simple(options, "-l");
 	if (option)
 	{
-		display_list(list);
+		display_list(list_all);
 		ft_strdel(&option);
 	}
-	option = ft_optgetopt_simple(options, "-d");
-	if (option)
-	{
-			all = 1;
-			ft_strdel(&option);
-	}
-	else
-			all = 0; // depile
+    list = recup_list(options, list_all);
+
+    display_list(list);
 	ft_tabstrdel(list);
+	ft_tabstrdel(list_all);
 	ft_optdel(options);	
 	return (0);
 }
