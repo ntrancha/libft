11db57062c35f0aa882e5647c88a2f3c78acaad2
ntrancha <101010.nk@gmail.com>
2015-09-01 07:01:48 +0200

    select_lib

--- conf/select_lib.c
+++ conf/select_lib.c
@@ -6,7 +6,7 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/31 11:25:47 by ntrancha          #+#    #+#             */
-/*   Updated: 2015/09/01 06:52:56 by ntrancha         ###   ########.fr       */
+/*   Updated: 2015/09/01 07:01:24 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,125 +104,6 @@ char    **recup_list(t_opt *options, char **list_all)
         return (recup_src(options, list_all));
 	ft_strdel(&option);
     return (list_src());
-}
-
-int         ft_count_char(char *str, char c)
-{
-    int     index;
-    int     ret;
-
-    index = 0;
-    ret = 0;
-    while (str[index])
-        if (str[index++] == c)
-            ret++;
-    return (ret);
-}
-
-t_list      *get_proto(char *content, t_list *list)
-{
-    int     line;
-    int     count;
-    char    *ligne;
-
-    line = ft_count_char(content, '\n');
-    count = -1;
-    while (++count < line)
-    {
-        ligne = ft_strgetline(content, count);
-        if (ft_strcchr(ligne, ";") && ft_strcchr(ligne, "ft_"))
-            ft_listadd(list, (void*)ft_strdup(ligne));
-        ft_strdel(&ligne);
-    }
-    return (NULL);
-}
-
-int     test_file(char *file, char *path)
-{
-    char    **dos;
-    int     index;
-    int     ret;
-
-    dos = ft_getdirtab(path, NULL);
-    index = -1;
-    ret = 0;
-    while (dos[++index])
-    {
-        if (!ft_strcchr(dos[index], path))
-            ret = 1;
-    }
-    ft_tabstrdel(dos);
-    return (ret);
-}
-
-void    make_makefile(t_list *protos, int test, char *lib, char *output)
-{
-    t_node  *node;
-    t_list  *makefile;
-    char    *file;
-    char    *src;
-    char    *tmp;
-    char    *file_ok;
-    int     start;
-    int     end;
-
-    node = protos->start;
-    makefile = ft_listcreate();
-    while (node)
-    {
-        start = 0;
-        file = (char*) node->content;
-        while (file[start] && file[start] != ' ' && file[start] != '\t')
-            start++;
-        while (file[start] && (file[start] == ' ' || file[start] == '\t'))
-            start++;
-        while (file[start] && file[start + 1] && file[start] != 'f' && file[start + 1] != 't')
-            start++;
-        end = start;
-        while (file[end] && file[end] != '(' && file[end] != ';')
-            end++;
-        file_ok = ft_strsub(file, start, end - start);
-        if (test == 1)
-            tmp = ft_strmjoin("../src/", lib, "/");
-        else
-            tmp = ft_strmjoin("src/", lib, "/");
-        src = ft_strmjoin(tmp, file_ok, ".c");
-        if (test_file(file_ok, tmp))
-            ft_listadd(makefile, (void*)src);
-        ft_strdel(&tmp);
-        ft_strdel(&file_ok);
-        node = node->next;
-    }
-    ft_listdel(makefile, ft_memdel);
-}
-
-void    creation(char **list, char *output)
-{
-    char    *mini_lib;
-    char    *content;
-    t_list  *protos;
-    int     index;
-    int     test;
-
-    index = -1;
-    test = test_directory();
-    while (list[++index])
-    {
-		if (ft_strcmp(list[index], ".") != 0 && ft_strcmp(list[index], "..") != 0)
-        {
-            protos = ft_listcreate();
-            if (test == 1)
-                mini_lib = ft_strmjoin("../includes/", list[index], ".h");
-            else
-                mini_lib = ft_strmjoin("includes/", list[index], ".h");
-            content = ft_get_file(mini_lib);
-            get_proto(content, protos);
-            ft_strdel(&mini_lib);
-            ft_strdel(&content);
-            make_makefile(protos, test, list[index], output);
-            ft_listdel(protos, ft_memdel);
-        }
-    }
 }
 
 void		verif_file(char *lib, t_list *files, char **dir, char *output)
