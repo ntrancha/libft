b0bcd527b0174d5b6efdf71a0504cf43293a3077
ntrancha <101010.nk@gmail.com>
2015-09-06 10:35:45 +0200

    ft_strf.c

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/06 10:00:42 by ntrancha          #+#    #+#             */
-/*   Updated: 2015/09/06 10:33:49 by ntrancha         ###   ########.fr       */
+/*   Updated: 2015/09/06 10:35:38 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,6 @@ int		main(void)
 	printf("%.2Lf \n", result);
 	printf("%*.*Lf \n", 8, 2, result);  /* equivalent a   %8.2Lf    */
 	printf("\n");                     /* affichage du caractère % */
-	ft_putendl(ft_strf("%.daze", nbre));
+	ft_putendl(ft_strf("%2.4daze", nbre));
 	return 0;
 }
