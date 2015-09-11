aeec71c07c7e5cfcacaac902f22876a1eb117ac8
Ntrancha <101010.nk@gmail.com>
2015-09-11 09:55:16 +0200

    bits

--- src/bits/includes/bits.h
+++ src/bits/includes/bits.h
@@ -6,12 +6,13 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/09 06:32:25 by ntrancha          #+#    #+#             */
-/*   Updated: 2015/09/11 09:48:08 by ntrancha         ###   ########.fr       */
+/*   Updated: 2015/09/09 06:48:59 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITS_H
 # define BITS_H
+# include <stdlib.h>
 
 typedef unsigned char	t_bin;
 typedef unsigned char	t_octet;
@@ -25,18 +26,9 @@ typedef struct			s_bits
 
 t_bits	*ft_bitscreate(void *memory, size_t octet);
 t_bits	*ft_bitsdel(t_bits *memory);
-t_bin	ft_bitsgetoctet(t_bits *memory, int octet);
+t_octet	ft_bitsgetoctet(t_bits *memory, int octet);
 t_octet	*ft_bitsgetaddr(t_bits *memory, int octet);
-int 	ft_bitsgetbit(t_bin a, int bit);
-t_bin   ft_bits_or(t_bin a, t_bin b);
-t_bin   ft_bits_xor(t_bin a, t_bin b);
-t_bin   ft_bits_and(t_bin a, t_bin b);
-t_bin   ft_bits_shr(t_bin a, int shr);
-t_bin   ft_bits_shl(t_bin a, int shl);
-t_bin   ft_bits_ror(t_bin a, int ror);
-t_bin   ft_bits_rol(t_bin a, int rol);
-t_bin   ft_bits_not(t_bin a);
-t_bin   ft_bitssetoctet(t_bits *memory, int n_octet, t_bin octet);
+t_octet ft_bitssetoctet(t_bits *memory, int n_octet, t_bin octet);
 int	    ft_bitssetbit(t_bits *memory, int octet, int bit, int value);
 
 #endif
