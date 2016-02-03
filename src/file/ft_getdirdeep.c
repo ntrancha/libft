ff66c3092e3cd4ccae1f7192e7c1fd47115a4716
Ntrancha <101010.nk@gmail.com>
2016-02-03 14:33:18 +0100

    ft_getdirtab_f

--- src/file/ft_getdirdeep.c
+++ src/file/ft_getdirdeep.c
@@ -6,26 +6,24 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/03 12:33:50 by ntrancha          #+#    #+#             */
-/*   Updated: 2016/02/03 13:40:49 by ntrancha         ###   ########.fr       */
+/*   Updated: 2016/02/03 14:17:02 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/file.h"
-#include "../../includes/put.h"
 #include "../../includes/mem.h"
 #include "../../includes/strings.h"
 
 int         ft_getdirdeep(char *path, char *error)
 {
     char    **files;
-    //char    *newfile;
     int     index;
     int     ret;
     int     tmp;
 
     ret = 0;
     if ((files = ft_getdirtab_f(path, error, 'd')) == NULL)
-        return (1);
+        return (0);
     index = -1;
     while (files[++index])
         if (ft_strcmp(files[index], ".") && ft_strcmp(files[index], ".."))
