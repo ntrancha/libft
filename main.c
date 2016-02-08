c1e764fcb44651c148c3ef9f16447a37635b4c86
Ntrancha <101010.nk@gmail.com>
2016-02-08 20:04:39 +0100

    jusqu'ici tout va bien ...

--- main.c
+++ main.c
@@ -6,31 +6,53 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/07 23:13:16 by ntrancha          #+#    #+#             */
-/*   Updated: 2016/02/08 19:30:07 by ntrancha         ###   ########.fr       */
+/*   Updated: 2016/02/08 20:04:25 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/libft.h"
 
+void        ft_mandisplay(char *lib)
+{
+    char    *path;
+    char    *content;
+    char    **tab;
+    int     index;
+
+    index = -1;
+    ft_putendl(lib);
+    path = ft_strmjoin("conf/man/", lib, ".man");
+    content = ft_get_file(path);
+    tab = ft_strsplit(content, '\n');
+    while (tab[++index])
+    {
+        ft_putendl(tab[index]);
+    }
+    ft_tabstrdel(tab);
+    ft_strdel(&path);
+    ft_strdel(&content);
+}
+
 void        ft_man(void)
 {
     int     status;
     char    *buffer;
+    char    *man;
     int     ret;
     int     col;
 
     status = 1;
     buffer = NULL;
     col = ft_atoi(GET(COL_PROG));
+    XSTR(libft, MAN_LIB);
     ft_putcenter("MAN LIBFT", col);
     ft_putendl("");
     while (status)
     {
+        ft_mandisplay(GET(MAN_LIB));
         ret = ft_readstdin(&buffer);
         if (ft_strcmp(buffer, "exit\n") == 0)
             status = 0;
-        ft_putstr(":");
-        ft_putstr(buffer);
         ft_strdel(&buffer);
     }
 }
