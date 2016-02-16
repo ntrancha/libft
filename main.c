13b63750d2cfd5fb642250258ef42c9fab07cb8d
Ntrancha <101010.nk@gmail.com>
2016-02-16 11:31:08 +0100

    jusqu'ici tout va bien ...

--- main.c
+++ main.c
@@ -6,34 +6,15 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/07 23:13:16 by ntrancha          #+#    #+#             */
-/*   Updated: 2016/02/16 11:20:22 by ntrancha         ###   ########.fr       */
+/*   Updated: 2016/02/16 11:31:02 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "includes/libft.h"
 
 int         ft_main(void)
 {
-    //ft_putnbr_endl((int)ft_get_timestamp());
-    //ft_putnbr_endl((int)ft_time());
     ft_stack_infos();
-    /*ft_putnbr_endl(ft_rand(1,10));
-    ft_putnbr_endl(ft_rand(1,10));
-    ft_putnbr_endl(ft_rand(1,10));
-    ft_putnbr_endl(ft_rand(1,10));
-    ft_putnbr_endl(ft_rand(1,100));
-    ft_putnbr_endl(ft_rand(1,100));
-    ft_putnbr_endl(ft_rand(1,100));
-    ft_putnbr_endl(ft_rand(1,100));
-    ft_putnbr_endl(ft_rand(1,100));
-    ft_putnbr_endl(ft_rand(1,100));
-    ft_putnbr_endl(ft_random());
-    ft_putnbr_endl(ft_random());
-    ft_putnbr_endl(ft_random());*/
     return 1;
 }
 
