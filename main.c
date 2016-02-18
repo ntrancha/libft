0b62a638b4fcae27da8442d18aaac57de9d48b54
Ntrancha <101010.nk@gmail.com>
2016-02-18 21:30:57 +0100

    jusqu'ici tout va bien ...

--- main.c
+++ main.c
@@ -6,21 +6,11 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/07 23:13:16 by ntrancha          #+#    #+#             */
-/*   Updated: 2016/02/18 19:41:29 by ntrancha         ###   ########.fr       */
+/*   Updated: 2016/02/18 21:30:41 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/libft.h"
-
-void        parent(void)
-{
-    ft_putendl("parent");
-}
-
-void        child(void)
-{
-    ft_putendl("enfant");
-}
 
 int         ft_main(void)
 {
@@ -34,20 +24,7 @@ int         ft_main(void)
     CSTR("-42", "test_convert");
     ft_alloc_cnvrt("test_convert", "int");
     ft_alloc_cnvrt("test_convert", "str");
-    str = ft_getkey();
-    ft_putnbr_endl((int)str[0]);
-    ft_putnbr_endl((int)str[1]);
-    ft_putnbr_endl((int)str[2]);
-    ft_strdel(&str);
     ft_stack_infos();
-    str = ft_read_psswd();
-    ft_putendl(str);
-    ft_strdel(&str);
-    fork = ft_fork_goto(parent, child);
-    if (fork != 0)
-        fork = ft_fork_goto(parent, child);
-    if (fork != 0)
-        ft_putnbr_endl(fork);
     return 1;
 }
 
