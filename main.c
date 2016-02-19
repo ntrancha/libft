2b69822592a8af01eaa5fcc0266c924d773b2fec
Ntrancha <101010.nk@gmail.com>
2016-02-19 21:31:02 +0100

    jusqu'ici tout va bien ...

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/07 23:13:16 by ntrancha          #+#    #+#             */
-/*   Updated: 2016/02/19 21:21:29 by ntrancha         ###   ########.fr       */
+/*   Updated: 2016/02/19 21:31:00 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,8 @@ int         ft_main(void)
 
     DBG_FILE;
     DBG_PROG;
-    ft_alloc_copy("OPTIONS", "OPTIONS2");
-    dos = ft_getdirtab(".", NULL);
-    ft_alloc(dos, ft_tabstrlen(dos), "TAB", "tabstr"); 
-    ft_alloc_fusion("DOS_PROG", "OPTION_1", "NEW");
-    ft_alloc_fusion("TAB", "TAB", "NEW_TAB");
     ptr = &ft_strlen;
-    ft_calloc(ptr, 48, "DUMP STACK", "mem");
+    ft_calloc(ptr, 96, "DUMP STACK", "mem");
     ft_stack_infos();
     return 1;
 }
