9e2f931a9a6b297f7bdb0436e820501075232434
Ntrancha <101010.nk@gmail.com>
2016-02-22 21:15:00 +0100

    jusqu'ici tout va bien ...

--- src/sys/ft_sysfunc_init.c
+++ src/sys/ft_sysfunc_init.c
@@ -6,12 +6,25 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/22 20:21:02 by ntrancha          #+#    #+#             */
-/*   Updated: 2016/02/22 20:30:57 by ntrancha         ###   ########.fr       */
+/*   Updated: 2016/02/22 21:13:52 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/sys.h"
 #include "../../includes/stack.h"
+
+static void     ft_tabstrunsort_void(void *ptr)
+{
+    t_alloc     *alloc;
+    char        **tab;
+
+    alloc = (t_alloc*)ptr;
+    if (alloc && alloc->type && ft_strcmp(alloc->type, "tabstr") == 0)
+    {
+        tab = (char**)alloc->content;
+        ft_tabstrsort(&tab, -1);
+    }
+}
 
 static void     ft_tabstrsort_void(void *ptr)
 {
@@ -29,4 +42,5 @@ static void     ft_tabstrsort_void(void *ptr)
 void            ft_sysfunc_init(void)
 {
     ft_sysfunc_add("tabstr", "sort", "vv*", ft_tabstrsort_void);
+    ft_sysfunc_add("tabstr", "unsort", "vv*", ft_tabstrunsort_void);
 }
