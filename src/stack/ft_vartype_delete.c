4aad1e03c1b0c5bf83a5eb52a30121d757298689
Ntrancha <101010.nk@gmail.com>
2016-02-22 21:11:11 +0100

    jusqu'ici tout va bien ...

--- src/stack/ft_vartype_delete.c
+++ src/stack/ft_vartype_delete.c
@@ -6,7 +6,7 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/07 00:41:00 by ntrancha          #+#    #+#             */
-/*   Updated: 2016/02/07 00:42:21 by ntrancha         ###   ########.fr       */
+/*   Updated: 2016/02/22 21:11:08 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,23 @@
 #include "../../includes/mem.h"
 #include "../../includes/strings.h"
 
+static void ft_func_free(t_funcs *func)
+{
+    t_funcs *next;
+
+    while (func)
+    {
+        if (func->name)
+            ft_strdel(&(func->name));
+        if (func->func_name)
+            ft_strdel(&(func->name));
+        if (func->func)
+            ft_memdel((void**)&func->func);
+        next = func->next;
+        ft_memdel((void**)&func);
+        func = next;
+    }
+}
 void        *ft_vartype_delete(t_type *var)
 {
     t_type  *next;
@@ -21,6 +38,8 @@ void        *ft_vartype_delete(t_type *var)
     if (var)
     {
         next = var->next;
+        if (var->func)
+            ft_func_free(var->func);
         if (var->type)
             ft_strdel(&(var->type));
         ft_memdel((void**)&var);
