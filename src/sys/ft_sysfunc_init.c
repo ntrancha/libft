e5f5849338ed8b7fed23c049f42d15d1bc74af08
Ntrancha <101010.nk@gmail.com>
2016-02-22 21:31:43 +0100

    jusqu'ici tout va bien ...

--- src/sys/ft_sysfunc_init.c
+++ src/sys/ft_sysfunc_init.c
@@ -6,7 +6,7 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/22 20:21:02 by ntrancha          #+#    #+#             */
-/*   Updated: 2016/02/22 21:27:42 by ntrancha         ###   ########.fr       */
+/*   Updated: 2016/02/22 21:31:23 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 #include "../../includes/sysfunc.h"
 #include "../../includes/stack.h"
 
-void            ft_sysfunc_init(void)
+void    ft_sysfunc_init(void)
 {
-    if (ft_alloc_get("FUNC_TABSTR"))
-        ft_tabstr_init();
+    ft_tabstr_init();
 }
