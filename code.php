f504e1c61682d5a9601a10b8bc0cd1251ba487b7
Ntrancha <101010.nk@gmail.com>
2016-02-27 23:56:48 +0100

    jusqu'ici tout va bien ...

--- code.php
+++ code.php
@@ -1,13 +1,19 @@
-<?
-    //TEST
-    name_variable = "test42";
+<?              /*CODE NK*/
+
+    new tabstr tableau = |23|12|42|32|;
+    new str name_variable = "test42";
+
     test42 = "COUCOU LES ENFANTS";
     test = "COUCOU LES LAPINS";
     test2 = 4;
 
     ${name_variable}.put();
-    test2.convert(mem)
-
+    test2.convert(mem);
+    LEN = test.len();
+    test.tolower();
+    test.replace(les, los);
+    test.toupper();
+    test.replace(LAPINS, LAPINO);
     #INFOS;
 
     #DBG_PROG;
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/07 23:13:16 by ntrancha          #+#    #+#             */
-/*   Updated: 2016/02/26 09:30:00 by ntrancha         ###   ########.fr       */
+/*   Updated: 2016/02/27 23:55:57 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 int         ft_main(void)
 {
-    ft_syscmd_file("code.php");
+    char    *str;
+
+    str = ft_strdup("33421331231243321331233");
+    ft_strreplace(&str, "33", "00", -1);
+    ft_putendl(str);
+    /*ft_syscmd_file("code.php");*/
     return 1;
 }
 
