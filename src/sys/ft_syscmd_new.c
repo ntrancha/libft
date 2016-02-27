ad864f78f8888b6f436921222b2b7a1e28a15639
Ntrancha <101010.nk@gmail.com>
2016-02-27 10:41:10 +0100

    jusqu'ici tout va bien ...

--- src/sys/ft_syscmd_new.c
+++ src/sys/ft_syscmd_new.c
@@ -6,7 +6,7 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/26 09:17:28 by ntrancha          #+#    #+#             */
-/*   Updated: 2016/02/26 15:32:18 by ntrancha         ###   ########.fr       */
+/*   Updated: 2016/02/27 10:40:48 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,28 @@ static void ft_syscmd_new_other(char *var, char *type, char *content)
     }
 }
 
-int         ft_syscmd_new(char *str)
+static int  ft_syscmd_testok(char *var)
 {
-    char    **tab;
+    if (ft_alloc_get(var) != NULL)
+        return (1);
+    return (0);
+}
+
+static int  ft_syscmd_test(char **tab)
+{
     char    **tab2;
     char    *type;
     char    *var;
     char    *content;
+    int     ret;
 
-    tab = ft_strsplit(str, '=');
     type = ft_strsub(tab[0], 4, ft_strlen(tab[0]) - 5);
-    tab2 = ft_strsplit(type, ' ');
+    if (!(tab2 = ft_strsplit(type, ' ')))
+        return (0);
     ft_strdel(&type);
-    var = ft_strsub(tab2[1], 0, ft_strlen(tab2[1]));
+    if ((var = ft_strsub(tab2[1], 0, ft_strlen(tab2[1]))))
+        if (ft_syscmd_testok(var))
+            ft_alloc_del(var);
     type = ft_strsub(tab2[0], 0, ft_strlen(tab2[0]));
     content = ft_strsub(tab[1], 1, ft_strlen(tab[1]) - 1);
     if (ft_strcmp(type, "str") == 0)
@@ -49,8 +58,25 @@ int         ft_syscmd_new(char *str)
         ft_sysint_alloc(ft_atoi(content), var);
     else
         ft_syscmd_new_other(var, type, content);
+    ret = ft_syscmd_testok(var);
     ft_strdelth(&type, &var, &content);
-    ft_tabstrdel(&tab);
     ft_tabstrdel(&tab2);
-    return (1);
+    return (ret);
+}
+
+int         ft_syscmd_new(char *str)
+{
+    char    **tab;
+    int     ret;
+
+    if (str && ft_strcchr(str, "=") != 0)
+    {
+        tab = ft_strsplit(str, '=');
+        if (!tab)
+            return (0);
+        ret = ft_syscmd_test(tab);
+        ft_tabstrdel(&tab);
+        return (ret);
+    }
+    return (0);
 }
