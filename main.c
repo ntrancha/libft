7563b5a56156c07927366e8088095c22900ce7b9
Ntrancha <101010.nk@gmail.com>
2016-02-27 23:57:03 +0100

    jusqu'ici tout va bien ...

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/07 23:13:16 by ntrancha          #+#    #+#             */
-/*   Updated: 2016/02/27 23:55:57 by ntrancha         ###   ########.fr       */
+/*   Updated: 2016/02/27 23:56:58 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int         ft_main(void)
     str = ft_strdup("33421331231243321331233");
     ft_strreplace(&str, "33", "00", -1);
     ft_putendl(str);
-    /*ft_syscmd_file("code.php");*/
+    ft_syscmd_file("code.php");
     return 1;
 }
 
