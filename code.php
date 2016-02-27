bf13a1e9488d367246092cd3fdbe6cb8a97e7560
Ntrancha <101010.nk@gmail.com>
2016-02-27 16:29:02 +0100

    jusqu'ici tout va bien ...

--- code.php
+++ code.php
@@ -1,10 +1,12 @@
 <?
     //TEST
-    name_variable = test42;
-
-    test42 = "OK";
+    name_variable = "test42";
+    test42 = "COUCOU LES ENFANTS";
+    test = "COUCOU LES LAPINS";
+    test2 = 4;
 
     ${name_variable}.put();
+    test2.convert(mem)
 
     #INFOS;
 
