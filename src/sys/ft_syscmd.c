0bd5fb5e993641eb921f4eb776966bc1ae71db10
Ntrancha <101010.nk@gmail.com>
2016-02-29 21:18:13 +0100

    jusqu'ici tout va bien ...

--- src/sys/ft_syscmd.c
+++ src/sys/ft_syscmd.c
@@ -6,13 +6,56 @@
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/26 09:06:43 by ntrancha          #+#    #+#             */
-/*   Updated: 2016/02/29 20:55:29 by ntrancha         ###   ########.fr       */
+/*   Updated: 2016/02/29 21:14:24 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/stack.h"
 #include "../../includes/strings.h"
 #include "../../includes/sys.h"
+
+static void ft_syscmd_replace(char **str, char *var)
+{
+    char    *old;
+    char    *new;
+    t_alloc *alloc;
+
+    alloc = ft_alloc_get(var);
+    if (alloc && ft_strcmp(alloc->type, "str") == 0)
+    {
+        old = ft_strmjoin("${", var, "}");
+        new = ft_strdup(alloc->content);
+        ft_strreplace(str, old, new, -1);
+        ft_strdelt(&old, &new);
+    }
+}
+
+static void ft_syscmd_transforme(char **str)
+{
+    char    *src;
+    char    *new;
+    int     index;
+    int     start;
+    int     end;
+
+    src = *str;
+    index = -1;
+    start = 0;
+    end = 0;
+    if (ft_strcchr(src, "${") != 0 && ft_strcchr(src, "}") != 0)
+    {
+        while (src[++index] && end == 0)
+            if (start == 0 && src[index] == '$' && src[index + 1] == '{')
+                start = index + 2;
+            else if (start != 0 && end == 0 && src[index] == '}')
+                end = index;
+        new = ft_strsub(src, start, end - start);
+        ft_syscmd_replace(str, new);
+        ft_strdel(&new);
+    }
+    if (end)
+        ft_syscmd_transforme(str);
+}
 
 static void ft_syscmd__type(char *str)
 {
@@ -47,7 +90,10 @@ void        *ft_syscmd(char *str)
         ft_tabstrdel(&tab);
     }
     else
+    {
+        ft_syscmd_transforme(&tmp);
         ft_syscmd__type(tmp);
+    }
     ft_strdel(&tmp);
     return (str);
 }
