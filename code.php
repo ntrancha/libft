3147eb2a73ddc17fa8083a2de460d8c5a29bdcd2
Ntrancha <101010.nk@gmail.com>
2016-02-29 21:26:47 +0100

    jusqu'ici tout va bien ...

--- code.php
+++ code.php
@@ -1,14 +1,13 @@
 <?              /*CODE NK*/
 
     new tabstr tableau = ["23","12","42","32"];
-    new str name_variable = "test42";
-    new str test42 = "ok les lapins";
-
-
-    ${name_variable}.put();
+    new string name_variable = "test42";
+    new string test42 = "ok les lapins";
+    new int coc = 42;
     test = test42;
-    test2 = test
+    test2 = test;
     test2.convert(mem);
+    ${name_variable}.put();
     LEN = test.len();
     test.tolower();
     test.replace(les, los);
